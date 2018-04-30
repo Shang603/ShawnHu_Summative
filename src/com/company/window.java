a9a8313ee49e8618835ffd5089c40db67addad46
Shawn <shawnhu97@gmail.com>
2018-04-30 19:47:31 -0400

    the fuck

--- src/com/company/window.java
+++ src/com/company/window.java
@@ -9,7 +9,7 @@ public class window extends JFrame {
 
     ImageIcon FJap = new ImageIcon(getClass().getResource("japanese town.gif"));
     Wizard wiz = new Wizard();
-    String hugeMonster = "hi";
+    String thefuck = "hi";
 
     public window(){
 
