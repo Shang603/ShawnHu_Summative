a1de3bcdc0a4c9ed3b5afc5f18bcf750e0be2ea1
Shawn <shawnhu97@gmail.com>
2018-05-01 14:12:02 -0400

    new update

--- src/com/company/window.java
+++ src/com/company/window.java
@@ -1,27 +1,42 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class window extends JFrame {
 
     static int width = 1300;
     static int height = 700;
 
+    JLabel background = new JLabel();
+
     ImageIcon FJap = new ImageIcon(getClass().getResource("japanese town.gif"));
     Wizard wiz = new Wizard();
 
-    public window(){
+    public window() {
 
-        setSize(width,height);
+        setSize(width, height);
         setLayout(null);
         setUndecorated(true);
-        add(wiz.icon);
+        setLocationRelativeTo(null);
 
+
+        background.setIcon(imgRescaler(FJap, width, height));
+        background.setBounds(0, 0, width, height);
+
+        add(wiz.icon);
+        add(background);
 
 
     }
 
+    ImageIcon imgRescaler(ImageIcon img, int w, int h) {
 
+        Image tempImg = img.getImage();
+        ImageIcon tempFinal = new ImageIcon(tempImg.getScaledInstance(w, h, Image.SCALE_DEFAULT));
+        return tempFinal;
+
+    }
 
 
 }
